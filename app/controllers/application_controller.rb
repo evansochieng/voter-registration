138c1b7e0cfc3c72c77ec25c852ac5d5952da05e
evans ochieng <evansochieng121@gmail.com>
2022-11-07 12:26:35 +0300

    seed db and try querying db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,20 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/wards" do
+    #get all wards 
+    wards = Ward.all
+
+    #return json array with the names only
+    wards.to_json(only: :name)
+  end
+
+  get "/pollingstation" do
+    #get all polling stations
+    polling_stations = PollingStation.all
+
+    #return json array
+    polling_stations.to_json
   end
 
 end
