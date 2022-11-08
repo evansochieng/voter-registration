f93bb3d98817507feaf76657d7478c929c802902
evans ochieng <evansochieng121@gmail.com>
2022-11-08 17:41:24 +0300

    create endpoints for summary stats of voters per ps

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+  # Routing
+  # Get an array of all wards
   get "/wards" do
     #get all wards 
     wards = Ward.all
@@ -10,28 +11,37 @@ class ApplicationController < Sinatra::Base
     wards.to_json(only: :name)
   end
 
-  get "/pollingstation" do
+  # Get an array of all polling stations
+  get "/pollingstations" do
     #get all polling stations
     polling_stations = PollingStation.all
 
     #return json array
     polling_stations.to_json
   end
+
+  # Get an array of all voters
   get "/voters" do
     #get all voters
     voters = Voter.all
+
+    #return JSON
     voters.to_json
   end
 
+  # Search voter by id
   get '/voters/:id' do
     #get voter by id
-    voters = Voter.find(params[:id])
-    voters.to_json
+    voter = Voter.find(params[:id])
+
+    #return JSON
+    voter.to_json
   end
 
+  # Persist voter info on registration
   post '/voters' do
     #add a new voter
-    voters = Voter.create(
+    voter = Voter.create(
       first_name: params[:first_name],
       middle_name: params[:middle_name],
       last_name: params[:last_name],
@@ -41,29 +51,65 @@ class ApplicationController < Sinatra::Base
       id_number: params[:id_number],
       polling_station_id: params[:polling_station_id]
     )
-    voters.to_json
+    #return JSON of the new voter info
+    voter.to_json
   end
 
-  delete '/voters/:id' do
-    #delete a voter
-    voters = Voter.find(params[:id])
-    voters.destroy
-  end
-
+  #Update voter details
   patch '/voters/:id' do 
     #a user can change their details
-    voters = Voter.find(params[:id])
-    voters.update(
-      first_name: params[:first_name],
-      middle_name: params[:middle_name],
-      last_name: params[:last_name],
-      age: params[:age],
-      gender: params[:gender],
-      DOB: params[:DOB],
-      id_number: params[:id_number],
-      polling_station_id: params[:polling_station_id]
-    )
-    voters.to_json
+    voter = Voter.find(params[:id])
+
+    #Update voter details
+    # voter.update(
+    #   first_name: params[:first_name],
+    #   middle_name: params[:middle_name],
+    #   last_name: params[:last_name],
+    #   age: params[:age],
+    #   gender: params[:gender],
+    #   DOB: params[:DOB],
+    #   id_number: params[:id_number],
+    #   polling_station_id: params[:polling_station_id]
+    # )
+    voter.update(params)
+
+    # return JSON
+    voter.to_json
+  end
+
+  # Delete voter info
+  delete '/voters/:id' do
+    #delete a voter
+    voter = Voter.find(params[:id])
+
+    # Delete voter from database
+    voter.destroy
+  end
+
+  ##GET SUMMARY OF VOTERS IN POLLING STATIONS
+  ## This will be displayed on home screen
+  get '/pollingstations/votercounts' do
+    #Array to store the counts
+    voters_summary = []
+    #voters_per_ps = Voter.group(:polling_station_id).count
+    
+    #find the number of voters per polling station
+    PollingStation.all.each do |ps|
+      voter_counts = {"#{ps.name}": ps.voters.count}
+      voters_summary << voter_counts
+    end
+
+    #return JSON array with name and count
+    voters_summary.to_json
+  end
+
+  #return polling stations
+  get '/pollingstations/:id' do
+    #get all voters in a particular polling station
+    ps_voters = PollingStation.find(params[:id]).voters
+
+    #return only voter's names, id_number
+    ps_voters.to_json(only: [:first_name, :middle_name, :last_name, :id_number])
   end
 
 end
